b84fce169ded72bcdc95b11248e5decbd15a8ef0
Tendol <tendol@Tenzins-MacBook-Pro-2.local>
2023-09-11 20:13:47 -0400

    aded heroku host

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["peykhang.herokuapp.com", "www.peykhang.com"]
+ALLOWED_HOSTS = [
+    "peykhang.herokuapp.com",
+    "www.peykhang.com",
+    "peykhang-e0422ac066cb.herokuapp.com",
+]
 
 
 # Application definition
